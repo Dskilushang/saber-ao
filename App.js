a298cc6068f3898f1e1b4cfc2fb64becb7189c3e
Dskilushang <danielkilusisa@gmail.com>
2026-09-14 01:44:38 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import SplashScreen from './src/screens/SplashScreen';
 import AccueilScreen from './src/screens/AccueilScreen';
-import CategoriesScreen from './src/screens/CategoriesScreen';
 import QuizScreen from './src/screens/QuizScreen';
 import ResultatScreen from './src/screens/ResultatScreen';
 
@@ -14,14 +14,31 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Splash"
-        screenOptions={{ headerShown: false, animation: 'fade' }}
+        screenOptions={{
+          headerShown: false,
+          animation: 'fade',
+        }}
       >
-        <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="Accueil" component={AccueilScreen} />
-        <Stack.Screen name="Categories" component={CategoriesScreen} />
-        <Stack.Screen name="Quiz" component={QuizScreen} />
-        <Stack.Screen name="Resultat" component={ResultatScreen} />
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+        />
+
+        <Stack.Screen
+          name="Accueil"
+          component={AccueilScreen}
+        />
+
+        <Stack.Screen
+          name="Quiz"
+          component={QuizScreen}
+        />
+
+        <Stack.Screen
+          name="Resultat"
+          component={ResultatScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-                       }
+}
